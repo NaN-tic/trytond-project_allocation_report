b404a527a807c0d153183ce5384abf7a2cf7b422
Albert Cervera i Areny <albert@nan-tic.com>
2014-05-19 18:11:49 +0200

    Convert field from Char to Many2One and add the setter function.

--- work.py
+++ work.py
@@ -9,14 +9,30 @@ __metaclass__ = PoolMeta
 
 class Work:
     __name__ = 'project.work'
-    assigned_employees = fields.Function(fields.Char('Assigned'),
-        'get_assigned_employees', searcher='search_assigned_employees')
+    assigned_employee = fields.Function(fields.Many2One('company.employee',
+            'Assigned'), 'get_assigned_employee',
+        setter='set_assigned_employee', searcher='search_assigned_employee')
 
-    def get_assigned_employees(self, name):
-        return ",".join([x.employee.rec_name for x in self.allocations])
+    def get_assigned_employee(self, name):
+        if self.allocations:
+            return self.allocations[0].employee.id
 
     @classmethod
-    def search_assigned_employees(cls, name, clause):
+    def set_assigned_employee(cls, works, name, value):
+        Allocation = Pool().get('project.allocation')
+        Allocation.delete([allocation for work in works
+                for allocation in work.allocations])
+        if value:
+            to_create = []
+            for work in works:
+                to_create.append({
+                        'employee': value,
+                        'work': work.id,
+                        })
+            Allocation.create(to_create)
+
+    @classmethod
+    def search_assigned_employee(cls, name, clause):
         if clause[2] is None:
             return [('allocations',) + tuple(clause[1:])]
         return [('allocations.employee',) + tuple(clause[1:])]
